0dfcaf4a4decd649490beb8bb0f4e1fd08324adf
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-06-13 07:36:15 -0400

    Update AbstractToiletBlock.java
    Added clarity to plumbing system, as well allowing a toilet to be used with an item in hand as well!

--- src/main/java/com/srcfur/badhygiene/blocks/custom/AbstractToiletBlock.java
+++ src/main/java/com/srcfur/badhygiene/blocks/custom/AbstractToiletBlock.java
@@ -10,8 +10,12 @@ import com.srcfur.badhygiene.fluids.ModFluids;
 import com.srcfur.badhygiene.fluids.UrineFluid;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -80,11 +84,18 @@ public abstract class AbstractToiletBlock extends BaseEntityBlock {
                         }
                     } else {
                         //Add thingy to make it visible a toilet is backed up
+                        player.sendSystemMessage(Component.literal("Toilet is backed up, consider plumbing it!"));
+                        return InteractionResult.FAIL;
                     }
                 }
                 return InteractionResult.SUCCESS;
             }
         }
         return super.useWithoutItem(blockState, level, blockPos, player, blockHitResult);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        return useWithoutItem(state, level, pos, player, hitResult) == InteractionResult.SUCCESS ? ItemInteractionResult.SUCCESS : ItemInteractionResult.FAIL;
     }
 }
