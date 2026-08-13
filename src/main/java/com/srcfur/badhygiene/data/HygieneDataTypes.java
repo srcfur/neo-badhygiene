855b7d1f3848d1de5f25eb26737c68b397aa3c5e
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-08-12 20:20:30 -0400

    Adding messing mixin

---
+++ src/main/java/com/srcfur/badhygiene/data/HygieneDataTypes.java
@@ -0,0 +1,24 @@
+package com.srcfur.badhygiene.data;
+
+import com.mojang.serialization.Codec;
+import com.srcfur.badhygiene.BadHygiene;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(BadHygiene.MODID)
+public class HygieneDataTypes {
+    public HygieneDataTypes(IEventBus modbus){
+        DATA_COMPONENTS.register(modbus);
+    }
+    public static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, BadHygiene.MODID);
+
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> HYGIENE_SOILED_CLOTHING = DATA_COMPONENTS.registerComponentType(
+            "soiled",
+            booleanBuilder -> booleanBuilder.networkSynchronized(ByteBufCodecs.BOOL).persistent(Codec.BOOL));
+}
